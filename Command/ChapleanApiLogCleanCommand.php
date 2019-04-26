44a22726bc4b1339157ee0b9d5a314d2e58d77f3
Nicolas <nicolas@chaplean.coop>
2019-04-26 18:07:17 +0200

    Improve logs clear command

--- Command/ChapleanApiLogCleanCommand.php
+++ Command/ChapleanApiLogCleanCommand.php
@@ -3,7 +3,8 @@
 namespace Chaplean\Bundle\ApiClientBundle\Command;
 
 use Chaplean\Bundle\ApiClientBundle\Utility\ApiLogUtility;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -12,10 +13,25 @@ use Symfony\Component\Console\Output\OutputInterface;
  *
  * @package   Chaplean\Bundle\ApiClientBundle\Command
  * @author    Hugo - Chaplean <hugo@chaplean.com>
- * @copyright 2014 - 2018 Chaplean (http://www.chaplean.com)
+ * @copyright 2014 - 2018 Chaplean (https://www.chaplean.com)
  */
-class ChapleanApiLogCleanCommand extends ContainerAwareCommand
+class ChapleanApiLogCleanCommand extends Command
 {
+    /** @var ApiLogUtility */
+    protected $apiLogUtility;
+
+    /**
+     * ChapleanApiLogCleanCommand constructor.
+     *
+     * @param ApiLogUtility $apiLogUtility
+     */
+    public function __construct(ApiLogUtility $apiLogUtility)
+    {
+        parent::__construct();
+
+        $this->apiLogUtility = $apiLogUtility;
+    }
+
     /**
      * Configures the current command.
      *
@@ -24,7 +40,12 @@ class ChapleanApiLogCleanCommand extends ContainerAwareCommand
     protected function configure()
     {
         $this->setName('chaplean:api-log:clean');
-        $this->setDescription('Delete logs older than one month');
+        $this->setDescription('Delete logs older than the mentionned date (1 month by default)');
+        $this->addArgument(
+            'minimumDate',
+            InputArgument::OPTIONAL,
+            'The logs dated before this date will be removed.'
+        );
     }
 
     /**
@@ -34,14 +55,16 @@ class ChapleanApiLogCleanCommand extends ContainerAwareCommand
      * @param OutputInterface $output
      *
      * @return void
+     *
+     * @throws \Exception
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        /** @var ApiLogUtility $apiLogUtility */
-        $apiLogUtility = $this->getContainer()->get(ApiLogUtility::class);
+        $strDate = $input->getArgument('minimumDate') ?: 'now - 1 month midnight';
+        dump($strDate);
+        $dateLimit = new \DateTime($strDate);
 
-        $dateLimit = new \DateTime('now - 1 month midnight');
-        $apiLogsDeleted = $apiLogUtility->deleteMostRecentThan($dateLimit);
+        $apiLogsDeleted = $this->apiLogUtility->deleteMostRecentThan($dateLimit);
 
         $output->writeln($apiLogsDeleted . ' logs removed');
     }
--- Tests/Command/ChapleanApiLogCleanCommandTest.php
+++ Tests/Command/ChapleanApiLogCleanCommandTest.php
@@ -5,8 +5,8 @@ namespace Tests\Chaplean\Bundle\ApiClientBundle\Command;
 use Chaplean\Bundle\ApiClientBundle\Command\ChapleanApiLogCleanCommand;
 use Chaplean\Bundle\ApiClientBundle\Utility\ApiLogUtility;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
-use Symfony\Component\Console\Input\ArgvInput;
-use Symfony\Component\Console\Output\NullOutput;
+use Mockery\MockInterface;
+use Symfony\Component\Console\Tester\CommandTester;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -14,33 +14,73 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *
  * @package   Tests\Chaplean\Bundle\ApiClientBundle\Command
  * @author    Hugo - Chaplean <hugo@chaplean.com>
- * @copyright 2014 - 2018 Chaplean (http://www.chaplean.com)
+ * @copyright 2014 - 2018 Chaplean (https://www.chaplean.com)
  */
 class ChapleanApiLogCleanCommandTest extends MockeryTestCase
 {
+    /**
+     * @var ApiLogUtility|MockInterface
+     */
+    protected $apiLogUtility;
+
+    /**
+     * @var ChapleanApiLogCleanCommand
+     */
+    private $command;
+
+    /**
+     * @var CommandTester
+     */
+    private $commandTester;
+
+    /**
+     * @return void
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->apiLogUtility = \Mockery::mock(ApiLogUtility::class);
+
+        $this->command = new ChapleanApiLogCleanCommand($this->apiLogUtility);
+        $this->commandTester = new CommandTester($this->command);
+    }
+
     /**
      * @covers \Chaplean\Bundle\ApiClientBundle\Command\ChapleanApiLogCleanCommand::configure()
      * @covers \Chaplean\Bundle\ApiClientBundle\Command\ChapleanApiLogCleanCommand::execute()
      *
      * @return void
+     *
+     * @throws \Exception
      */
-    public function testExecute()
+    public function testExecuteWithDefaultDate()
     {
-        $mockApiLogUtility = \Mockery::mock(ApiLogUtility::class);
+        $this->apiLogUtility->shouldReceive('deleteMostRecentThan')->once()->andReturn(1);
 
-        $container = \Mockery::mock(ContainerInterface::class);
-        $container->shouldReceive('get')
-            ->once()
-            ->with(ApiLogUtility::class)
-            ->andReturn($mockApiLogUtility);
+        $this->commandTester->execute([]);
 
-        $mockApiLogUtility->shouldReceive('deleteMostRecentThan')->andReturn(15);
+        $output = $this->commandTester->getDisplay();
 
-        $command = new ChapleanApiLogCleanCommand();
-        $command->setContainer($container);
+        $this->assertContains('1 logs removed', $output);
+    }
 
-        $input = new ArgvInput([]);
-        $output = new NullOutput();
-        $command->run($input, $output);
+    /**
+     * @covers \Chaplean\Bundle\ApiClientBundle\Command\ChapleanApiLogCleanCommand::configure()
+     * @covers \Chaplean\Bundle\ApiClientBundle\Command\ChapleanApiLogCleanCommand::execute()
+     *
+     * @return void
+     *
+     * @throws \Exception
+     */
+    public function testExecuteWithDate()
+    {
+        $this->apiLogUtility->shouldReceive('deleteMostRecentThan')->once()->andReturn(1);
+
+        $this->commandTester->execute(['minimumDate' => '-1 day']);
+
+        $output = $this->commandTester->getDisplay();
+
+        $this->assertContains('1 logs removed', $output);
     }
 }
